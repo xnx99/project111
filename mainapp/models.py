8825ae8a0a02c51ae5c019390415568d76d71a86
xnx99 <Noura4alsubaie@hotmail.com>
2018-06-28 07:07:19 +0300

    fresheyes update

--- mainapp/models.py
+++ mainapp/models.py
@@ -15,26 +15,42 @@ Categorization_choices = (
     ('P', u'physiology'),
 )
 
+#youtube model
+
 class Youtube(models.Model):
-    Title=models.TextField(max_length=500)
+    Title=models.TextField(max_length=75)
     uploadedby=models.ForeignKey(User ,null=True)
-    Description=models.TextField(max_length=500)
+    Description=models.TextField(max_length=150)
     Categorization=models.CharField(max_length=1, choices=Categorization_choices)
     Date= models.DateTimeField('date published',auto_now_add=True)
     is_deleted= models.BooleanField(verbose_name="هل الفديو محذوف؟",default=False)
     link=models.TextField(max_length=100)
 
-
+#telegram model
 
 class telegram_model(models.Model):
-    Title=models.TextField(max_length=250)
+    Title=models.TextField(max_length=75)
     uploadedby=models.ForeignKey(User ,null=True)
-    Description=models.TextField(max_length=350)
+    Description=models.TextField(max_length=150)
     Categorization=models.CharField(max_length=1, choices=Categorization_choices)
     Date= models.DateTimeField('date published',auto_now_add=True)
     is_deleted= models.BooleanField(verbose_name="هل القناة محذوف؟",default=False)
     link=models.TextField(max_length=100)
 
+#Fresh eyes model
+
+class fresheyes_model(models.Model):
+    Title=models.TextField(max_length=75)
+    uploadedby=models.ForeignKey(User ,null=True)
+    Description=models.TextField(max_length=150)
+    Categorization=models.CharField(max_length=1, choices=Categorization_choices)
+    Date= models.DateTimeField('date published',auto_now_add=True)
+    is_deleted= models.BooleanField(verbose_name="هل المستند محذوف؟",default=False)
+    file= models.FileField()
+
+
+    def __unicode__(self):
+        return unicode(self.file) or u''
 
 
 
